675615a0ca1c4de8013380ebf1883fff24a39b25
Daniel Carneros Mateu <109517814+theDn2000@users.noreply.github.com>
2022-12-14 16:22:57 +0100

    Update example.ps1

--- PS_Scripts/example.ps1
+++ PS_Scripts/example.ps1
@@ -12,4 +12,10 @@ param(
 
 )
 
+# Convert to system string
+
+$parameter1 = [System.Convert]::ToString($parameter1)
+
+$parameter2 = [System.Convert]::ToString($parameter2)
+
 "If this script were really going to do something, it would write parameter $parameter1, $parameter2 in working directory as log.txt" | Out-File -FilePath c:\PS_scripts_log\log.txt
