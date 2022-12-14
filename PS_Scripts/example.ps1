c003f13a7313d6ceec63c3d6716e81f3c825a3ad
Daniel Carneros Mateu <109517814+theDn2000@users.noreply.github.com>
2022-12-14 17:13:22 +0100

    Update example.ps1

--- PS_Scripts/example.ps1
+++ PS_Scripts/example.ps1
@@ -9,7 +9,15 @@ param(
     [Parameter()]
 
     [string]$parameter2
+    
+    
+    [Parameter()]
 
+    [string]$parameter3
+    
+    [Parameter()]
+
+    [string]$parameter4
 )
 
 # Convert to system string
@@ -18,4 +26,4 @@ $parameter1 = [System.Convert]::ToString($parameter1)
 
 $parameter2 = [System.Convert]::ToString($parameter2)
 
-"If this script were really going to do something, it would write parameter $parameter1, $parameter2 in working directory as log.txt" | Out-File -FilePath c:\PS_scripts_log\log.txt
+"If this script were really going to do something, it would write parameter $parameter1, $parameter2, $parameter3, $parameter4  in working directory as log.txt" | Out-File -FilePath c:\PS_scripts_log\log.txt
