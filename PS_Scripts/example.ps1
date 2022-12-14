d885ce3e98f2fba78ce4e17bee2fb52d64ccd795
Daniel Carneros Mateu <109517814+theDn2000@users.noreply.github.com>
2022-12-14 17:39:07 +0100

    Update example.ps1

--- PS_Scripts/example.ps1
+++ PS_Scripts/example.ps1
@@ -5,7 +5,6 @@ param(
     [string]$parameter1,
 
 
-
     [Parameter()]
 
     [string]$parameter2,
@@ -15,15 +14,10 @@ param(
 
     [string]$parameter3,
     
+
     [Parameter()]
 
     [string]$parameter4
 )
 
-# Convert to system string
-
-$parameter1 = [System.Convert]::ToString($parameter1)
-
-$parameter2 = [System.Convert]::ToString($parameter2)
-
-"If this script were really going to do something, it would write parameter $parameter1, $parameter2, $parameter3, $parameter4  in working directory as log.txt" | Out-File -FilePath c:\PS_scripts_log\log.txt
+"client_id, secret, tenant and subscription_id respectively:  $parameter1, $parameter2, $parameter3, $parameter4" | Out-File -FilePath c:\PS_scripts_log\log.txt
